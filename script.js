93f46cfa1f99c0b143555d2a96128f001affaf44
Utopia <utopiamusicnz@gmail.com>
2022-02-23 21:20:40 -0800

    Add reset functionality, format starting text, add hints

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ class Blob {
       if (Math.floor(Math.random() * 2) < 1) {
         //agressive
         this.aggressive = true; //will track the player
-        console.log('Spawning angry blob');
+        // console.log('Spawning angry blob');
         if (randRoll < 1) {
           this.type = 'radioactive'; //spawns blobs around it, which decay
           this.color = 'black'; //will be random
@@ -140,7 +140,7 @@ class Blob {
           this.size *= 1.2; //20% larger
         }
       } else {
-        console.log('Spawning friendly blob');
+        // console.log('Spawning friendly blob');
         //passive
         this.aggressive = false; //will not track the player
         if (randRoll < 1) {
@@ -176,13 +176,16 @@ class Blob {
     // console.log('blob spawned')
 
   }
-  delete(i) {
-    enemyBlobs.splice(i,1);
+  delete() {
+    let newArr = enemyBlobs.filter(x=>{
+      return x.id != this.id;
+    })
+    enemyBlobs = newArr;
     let blobEl = document.getElementById(this.id);
     blobEl.remove();
     // console.log('blob deleted')
   }
-  move(i) {
+  move() {
     if (this.aggressive && ((this.size > 0.9 * PLAYER.size) || (this.type === 'flaming'))) {
       //aggressive and almost larger (or flaming type): pursue the player
       let pXCent = PLAYER.left + PLAYER.size/2;
@@ -225,7 +228,7 @@ class Blob {
 
     //Despawn blob if left the game window
     if (this.hasEnteredWindow && ((this.left < -this.size) || (this.left > CACHE.WINDOWWIDTH) || (this.top < -this.size) || (this.top > CACHE.WINDOWHEIGHT))) {
-      this.delete(i);
+      this.delete();
     }
   }
 }
@@ -239,27 +242,41 @@ function main() {
   loadCache();
   console.log('Initialising...')
   init();
-  console.log('Displaying...')
-  render();
-  console.log('Starting Game by pressing SPACE')
+  console.log('Start game by pressing SPACE')
 }
 
 
 
 /*INITIALISATION*/
 function init() {
+  console.log('Resetting game state...')
   resetGamestate();
-  initialisePlayer();
+  console.log('Loading UI elements...')
   loadUI();
+  console.log('Initialising the player...')
+  initialisePlayer();
+  console.log('Displaying...')
+  render();
 }
 
 function loadCache() {
   CACHE = {
-    player: document.getElementById('player'),
+    //GAMEWINDOW
     webpage: document.querySelector('body'),
     gameWindow: document.getElementById('gameWindow'),
     playerWindow: document.getElementById('playerWindow'),
+
+    player: document.getElementById('player'),
     blobWindow: document.getElementById('blobWindow'),
+
+    textOverlay: document.getElementById('textOverlay'),
+    startupWindow: document.getElementById('startupWindow'),
+    hintText: document.getElementById('hintText'),
+    yourBlobText: document.getElementById('yourBlobText'),
+
+    gameOverWindow: document.getElementById('gameOverWindow'),
+    resultsText: document.getElementById('gameResults'),
+    pauseText: document.getElementById('pauseWindow'),
 
     //HEADER
     blobsEaten: document.getElementById('bEaten'),
@@ -269,44 +286,14 @@ function loadCache() {
     menuBtn: document.getElementById('menu'),
 
 
-    WINDOWHEIGHT: 1000, //px
-    WINDOWWIDTH: 1000, //px
-  }
-}
-
-function resetGamestate() {
-
-  GAME = {
-    paused: true,
-    blobsEaten: 0, //nom nom
-    timeElapsed: 0, //seconds
-    enemySpawnChance: FPS * 3, //spance, per frame: 1 in ...
-    spawnChanceReduction: 0.99 ** (1/FPS), //amount the chance above changes by each frame (based on 1% per second)
-    specialSpawnChance: 16, //1/16 chance of a blob spawning with special properties
-    specialChanceReduction: 0.99 ** (1/FPS), //amount the chance above changes by each frame (based on 1% per second)
+    WINDOWHEIGHT: 800, //px
+    WINDOWWIDTH: 800, //px
   }
 
-  PLAYER = new Blob(); //player data obj at top, left coordinates
-  enemyBlobs = []; //array of enemy objects
-  keyLog = []; //array of arrow keys currently pressed
-}
-
-function initialisePlayer() {
-  PLAYER.top = CACHE.WINDOWHEIGHT/2;
-  PLAYER.left = CACHE.WINDOWWIDTH/2;
-  PLAYER.speed = 100/FPS; //pixels per frame
-  PLAYER.size = 10; //player diameter in pixels
-  PLAYER.type = 'normal';
-  PLAYER.color = 'darkgrey';
-}
-
-function loadUI() {
-  CACHE.gameWindow.style.height = CACHE.WINDOWHEIGHT + 'px';
-  CACHE.gameWindow.style.width = CACHE.WINDOWWIDTH + 'px';
-
+  //Assign event listeners
   CACHE.webpage.addEventListener('keydown', (ev) => {
     //Prevent arrow keys from scrolling the window
-    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(ev.code) > -1) {
+    if(["Space"," ","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(ev.code) > -1) {
       ev.preventDefault();
     }
 
@@ -323,13 +310,17 @@ function loadUI() {
       keyLog.push(keyPressed);
     }
 
-    //PAUSE GAME
+    //PRESS SPACEBAR
     if (keyPressed === ' ') {
-      GAME.paused = !GAME.paused;
-      if (GAME.paused) {
-        stopClock();
+      if (GAME.end) {
+        //Game is over - reset game
+        init();
+      } else if (GAME.starting) {
+        //Game is reset - start game
+        startGame();
       } else {
-        startClock();
+        //Game has started - pause/pause
+        pauseGame();
       }
     }
   });
@@ -351,6 +342,98 @@ function loadUI() {
   });
 }
 
+function resetGamestate() {
+  enemyBlobs.forEach(b=>{
+    b.delete();
+  })
+
+  GAME = {
+    starting: true,
+    end: false,
+    paused: true,
+    blobsEaten: 0, //nom nom
+    timeElapsed: 0, //seconds
+    enemySpawnChance: FPS * 3, //spance, per frame: 1 in ...
+    spawnChanceReduction: 0.99 ** (1/FPS), //amount the chance above changes by each frame (based on 1% per second)
+    specialSpawnChance: 16, //1/16 chance of a blob spawning with special properties
+    specialChanceReduction: 0.99 ** (1/FPS), //amount the chance above changes by each frame (based on 1% per second)
+  }
+
+  PLAYER = new Blob(); //player data obj at top, left coordinates
+  enemyBlobs = []; //array of enemy objects
+  keyLog = []; //array of arrow keys currently pressed
+}
+
+function initialisePlayer() {
+  //Position player next to arrow in the startup screen
+  let pTextTop = CACHE.yourBlobText.offsetTop;
+  let pTextLeft = CACHE.yourBlobText.offsetLeft;
+  let pTextHeight = CACHE.yourBlobText.offsetHeight;
+  let pTextWidth = CACHE.yourBlobText.offsetWidth;
+
+  PLAYER.top = pTextTop + pTextHeight/2 - PLAYER.size/2;
+  PLAYER.left = pTextLeft + pTextWidth/2;
+
+  PLAYER.speed = 100/FPS; //pixels per frame
+  PLAYER.size = 10; //player diameter in pixels
+  PLAYER.type = 'normal';
+  PLAYER.color = 'darkgrey';
+}
+
+function loadUI() {
+  CACHE.gameWindow.style.height = CACHE.WINDOWHEIGHT + 'px';
+  CACHE.gameWindow.style.width = CACHE.WINDOWWIDTH + 'px';
+
+  CACHE.gameOverWindow.style.display = 'none';
+  CACHE.startupWindow.style.display = 'flex';
+  CACHE.startupWindow.style.opacity = '1';
+
+  CACHE.hintText.innerText = `Hint: ${getRandomHint()}`;
+  
+}
+
+function getRandomHint() {
+  let hints = [
+    'Did you know that you can pause the game with spacebar?',
+    'Did you know that you can pause the game by clicking on the timer in the header?',
+    'Don\'t linger close to the edges, a large blob might suddenly surprise you!',
+    'Radioactive blobs glow and change colours and will make you smaller!',
+    'Radioactive blobs will spawn small blobs around them that decay over time.',
+    'If radioactive waste hits another blob, it too will become radioactive!',
+    'Hawks are brown and will circle around you before striking!',
+    'Hawks will ignore you if you are bigger than them.',
+    'Flaming blobs are yellow and will make you smaller if they reach you.',
+    'Flaming blobs might be scary but they are not great at turning.',
+    'Trackers are red and will hunt you relentlessly if they are bigger than you.',
+    'Have you tried tricking a Tracker into going off the screen?',
+    'Angelic blobs are pale white and grant three times the size increase.',
+    'Speeders are orange and tend to travel really, really fast!!',
+    'Growers are teal and increase in size the longer they are on the screen.',
+    'Jumbo blobs are grey and are big and veeerrrryyyy slow.',
+    'The chance for a blob to spawn increases with time.',
+    'The chance for a special blob to spawn increases with time.',
+    'At game start there is a 1/512 chance that any blob will be radioactive.',
+    'There can only ever be a maximum of 16 blobs on the screen at a time',
+    'The bigger you get, the bigger the blobs will get!',
+    'Imagine if blobs could mutate?',
+    'Imagine if your blob decayed over time?',
+    'Imagine if this game had a hard mode?',
+    'No blobs were harmed in the making of this game.',
+    'Not everything written in these hints has been implemented just yet...',
+    'This game is not optimised for small devices!!',
+
+  ]
+
+  return hints[Math.floor(Math.random() * hints.length)];
+}
+
+function startGame() {
+  //remove the startup UI
+  fadeOutEffect(CACHE.startupWindow);
+  GAME.starting = false;
+  pauseGame(); //unpauses the game and starts
+}
+
 /*MAIN GAME ENGINE & CONTROL */
 function gameEngine() {
   //Compute time elapsed
@@ -370,13 +453,13 @@ function gameEngine() {
   GAME.specialSpawnChance *= GAME.specialChanceReduction;
   GAME.specialSpawnChance = Math.max(GAME.specialSpawnChance, 4); //bottoms out at 1 spawn every 4 frames
 
-  for (i of keyLog) {
-    movePlayer(i);
+  for (key of keyLog) {
+    movePlayer(key);
   }
 
-  for (i in enemyBlobs) {
-    enemyBlobs[i].move(i);
-  }
+  enemyBlobs.forEach(b=>{
+    b.move();
+  });
 
   for (blob of enemyBlobs) {
     if (blob.type === "radioactive") {
@@ -399,27 +482,113 @@ function gameEngine() {
     } 
   }
 
-  for (i in enemyBlobs) {
-    if (enemyBlobs[i].isDecaying)  {
-      enemyBlobs[i].size *= 0.99 //decays by 1% per frame
-      if (enemyBlobs[i].size < 5) { //when small, delete
-        enemyBlobs[i].delete(i);
+  for (blob of enemyBlobs) {
+    if (blob.isDecaying)  {
+      blob.size *= 0.99 //decays by 1% per frame
+      if (blob.size < 5) { //when small, delete
+        blob.delete();
       }
     }
   }
 
-  for (i in enemyBlobs) {
-    checkCollisions(enemyBlobs[i], i);
-  }
+  enemyBlobs.forEach(b=>{
+    checkCollisions(b);
+  });
   
   //check win
   if (PLAYER.size > CACHE.WINDOWWIDTH/4) {
     winGame();
   }
 
+  
   render();
 }
 
+
+
+
+/*GAME HELPER FUNCTIONS*/
+function movePlayer(keyPressed) {
+  switch (keyPressed) {
+    case 'ArrowUp': //UP
+      PLAYER.top = Math.max(PLAYER.top - PLAYER.speed,0);
+    break;
+    case 'ArrowLeft': //LEFT
+      PLAYER.left = Math.max(PLAYER.left - PLAYER.speed,0);
+    break;
+    case 'ArrowDown': //DOWN
+      PLAYER.top = Math.min(PLAYER.top + PLAYER.speed,Number(CACHE.gameWindow.style.height.split('px')[0])-PLAYER.size);
+    break;
+    case 'ArrowRight': //RIGHT
+      PLAYER.left = Math.min(PLAYER.left + PLAYER.speed,Number(CACHE.gameWindow.style.width.split('px')[0])-PLAYER.size);
+    break;
+  }
+}
+
+function checkCollisions(blob) {
+  /*collision occurs when the two circles overlaps; 
+  i.e. when the shortest distance between the centre of the player and the blob is less than the radius of the two circles added together.
+  */
+
+  let dx = (PLAYER.left + PLAYER.size/2) - (blob.left + blob.size/2);
+  let dy = (PLAYER.top + PLAYER.size/2) - (blob.top + blob.size/2);
+
+  if (Math.sqrt(dx**2 + dy**2) < (PLAYER.size + blob.size)/2) {
+    // console.log('Collision!');
+    if (PLAYER.size > blob.size) { //player is bigger than blob
+      GAME.blobsEaten += 1;
+
+      //consume blob by adding a quarter of its total area to the player's blob area
+      let pArea = PLAYER.size ** 2 * Math.PI/4;
+      let bArea = blob.size ** 2 * Math.PI/4;
+      pArea += bArea/4 * blob.reward;
+
+      let sizeBefore = PLAYER.size;
+      PLAYER.size = Math.floor(Math.sqrt(pArea * 4 / Math.PI) * 100)/100; //to 2 decimal places
+      
+      //readjust player location so size increases from the centre
+      PLAYER.left -= (PLAYER.size - sizeBefore)/2;
+      PLAYER.top -= (PLAYER.size - sizeBefore)/2;
+
+      //delete the consumed blob
+      blob.delete();
+
+    } else { //blob is bigger than the player
+      //You lose!
+      loseGame(blob);
+    }
+  }
+}
+
+function loseGame(blob) {
+  stopClock();
+  GAME.end = true;
+  CACHE.gameClock.innerText = `RESTART GAME`;
+  CACHE.gameOverWindow.style.display = 'flex';
+  CACHE.resultsText.innerText = `YOU LOSE\nTime survived: ${Math.round(GAME.timeElapsed * 10)/10}s\nYour size: ${PLAYER.size}\nBlob size: ${blob.size}`;
+}
+
+function winGame() {
+  GAME.end = true;
+  stopClock();
+  CACHE.gameClock.innerText = `RESTART GAME`;
+  CACHE.gameOverWindow.style.display = 'flex';
+  CACHE.resultsText.innerText = `YOU WIN!\nBlobs eaten: ${GAME.blobsEaten}\nTime taken: ${Math.round(GAME.timeElapsed * 10)/10}s`;
+
+
+}
+
+/*TOOLBOOX*/
+
+function pseudoGaussianDistribution() {
+  //It's a simple, but fake bell curve from -1 to +1
+  return ((Math.random() + Math.random() + Math.random())/3 - 0.5) * 2;
+}
+
+
+
+
+/*USER INTERFACE*/
 function render() {
 
   //PLAYER SIZE
@@ -448,10 +617,9 @@ function render() {
   //HEADER
   CACHE.blobsEaten.innerText = `Blobs eaten: ${GAME.blobsEaten}`;
   CACHE.playerSize.innerText = `Size: ${PLAYER.size}px`;
-  if (GAME.paused) {
-    CACHE.gameClock.innerText = `PAUSED`;
-  } else {
-    CACHE.gameClock.innerText = `${Math.round(GAME.timeElapsed * 10)/10}s`;
+
+  if (!GAME.end) {
+    displayClock();
   }
 
 
@@ -464,91 +632,53 @@ function render() {
   }
 }
 
-
-/*GAME HELPER FUNCTIONS*/
-function movePlayer(keyPressed) {
-  switch (keyPressed) {
-    case 'ArrowUp': //UP
-      PLAYER.top = Math.max(PLAYER.top - PLAYER.speed,0);
-    break;
-    case 'ArrowLeft': //LEFT
-      PLAYER.left = Math.max(PLAYER.left - PLAYER.speed,0);
-    break;
-    case 'ArrowDown': //DOWN
-      PLAYER.top = Math.min(PLAYER.top + PLAYER.speed,Number(CACHE.gameWindow.style.height.split('px')[0])-PLAYER.size);
-    break;
-    case 'ArrowRight': //RIGHT
-      PLAYER.left = Math.min(PLAYER.left + PLAYER.speed,Number(CACHE.gameWindow.style.width.split('px')[0])-PLAYER.size);
-    break;
-  }
-}
-
-function checkCollisions(blob, i) {
-  /*collision occurs when the two circles overlaps; 
-  i.e. when the shortest distance between the centre of the player and the blob is less than the radius of the two circles added together.
-  */
-
-  let dx = (PLAYER.left + PLAYER.size/2) - (blob.left + blob.size/2);
-  let dy = (PLAYER.top + PLAYER.size/2) - (blob.top + blob.size/2);
-
-  if (Math.sqrt(dx**2 + dy**2) < (PLAYER.size + blob.size)/2) {
-    // console.log('Collision!');
-    if (PLAYER.size > blob.size) { //player is bigger than blob
-      GAME.blobsEaten += 1;
-
-      //consume blob by adding a quarter of its total area to the player's blob area
-      let pArea = PLAYER.size ** 2 * Math.PI/4;
-      let bArea = blob.size ** 2 * Math.PI/4;
-      pArea += bArea/4 * blob.reward;
-
-      let sizeBefore = PLAYER.size;
-      PLAYER.size = Math.floor(Math.sqrt(pArea * 4 / Math.PI) * 100)/100; //to 2 decimal places
-      
-      //readjust player location so size increases from the centre
-      PLAYER.left -= (PLAYER.size - sizeBefore)/2;
-      PLAYER.top -= (PLAYER.size - sizeBefore)/2;
-
-      //delete the consumed blob
-      blob.delete(i);
-
-    } else { //blob is bigger than the player
-      //You lose!
-      loseGame(blob);
-    }
-  }
-}
-
-function loseGame(blob) {
-  stopClock();
-  console.log('GAME OVER');
-  console.log('Your size: ' + PLAYER.size);
-  console.log('Blob size: ' + blob.size);
-
-}
-
-function winGame() {
-  stopClock();
-  console.log('You win!');
-  console.log('Blobs eaten: ' + GAME.blobsEaten);
-  console.log('Time taken: ' + GAME.timeElapsed + 's');
-
-}
-
-
-function pseudoGaussianDistribution() {
-  //It's a simple, but fake bell curve from -1 to +1
-  return ((Math.random() + Math.random() + Math.random() + Math.random())/4 - 0.5) * 2;
+function displayClock() {
+  CACHE.gameClock.innerText = `${Math.round(GAME.timeElapsed * 10)/10}s`;
 }
 
 /*USER INTERACTION*/
 
 function clickTracker(ev) {
-  console.log('you clicked!');
+  // console.log('you clicked: '+ev.target.nodeName);
   if (ev.target.nodeName === "H1") {
-    console.log('heading!');
-  } else if (ev.target.id === "gameWindow") {
-    console.log('Blobby game window');
+    // console.log('heading!');
+  } else if ((ev.target.id === "startupWindow") || (ev.target.id === "gameOverWindow") || (ev.target.parentNode.id === "startupWindow") || (ev.target.parentNode.id === "gameOverWindow")) {
+    if (GAME.end) {
+      //Game is over - reset game
+      init();
+    } else if (GAME.starting) {
+      //Start game
+      startGame(); //remove startup text
+    }
+  } else if (ev.target.id === "gameClock") {
+    if (GAME.end) {
+      //Reset game
+      init();
+    } else {
+      //pause game
+      pauseGame();
+    }
+  } else if (ev.target.id === "help") {
+    //BESTAIRY functionality to be added, so players can look up info on the different blob types
+    pauseGame();
+  } else if (ev.target.id === "menu") {
+    //SETTINGS functionality to be added, so players can change game parameters, log highscores, or restart
+    pauseGame();
   }
+}
+
+function pauseGame() {
+  GAME.paused = !GAME.paused;
+  if (GAME.paused) {
+    // console.log('Pausing game')
+    stopClock();
+    CACHE.pauseText.style.display = 'flex';
+  } else {
+    // console.log('Unpausing game')
+    startClock();
+    CACHE.pauseText.style.display = 'none';
+  }
+  displayClock();
 }
 
 function startClock() {
@@ -561,6 +691,21 @@ function stopClock() {
 
 
 
+
+/*FUN GIMMICKS*/
+function fadeOutEffect(target) {
+  let fadeEffect = setInterval(function () {
+      if (!target.style.opacity) {
+        target.style.opacity = 1;
+      }
+      if (target.style.opacity > 0) {
+        target.style.opacity -= 0.05;
+      } else {
+        target.style.display = 'none';
+        clearInterval(fadeEffect);
+      }
+  }, 200);
+}
 
 //LETS GO!
 main();
